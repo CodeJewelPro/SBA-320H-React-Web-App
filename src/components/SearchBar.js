73c46fd9d9569bf71ab900002c1d57ec80220f01
CodeJewelPro <ducheinej@gmail.com>
2024-08-21 13:57:29 -0400

    commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,18 @@ const SearchBar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = () => {
-    if (searchTerm.trim()!=='');
+    if (searchTerm.trim()!==''){
+      console.log(`Searching for ${searchTerm}`);
       onSearch(searchTerm);
-    
+    }
   };
 
   return (
     <div className="search-bar">
       <input
         type="text"
+        id="search"
+        name="search"
         placeholder="Search characters"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
